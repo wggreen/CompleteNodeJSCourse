0318acae60b162d391226502d5e9df30b234aac7
William Green <wggreen.nss@gmail.com>
2024-10-01 13:13:23 -0400

    finished chapter 17

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,13 +4,24 @@ const getNotes = require('./notes.js')
 
 const command = process.argv[2]
 
-console.log(yargs.argv)
-
 yargs.command({
     command: "add",
     describe: "Add a new note",
-    handler: function (){
-        console.log('Adding a new note!')
+    builder:  {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        console.log('Title:', argv.title)
+        console.log('Body:', argv.body)
     }
 })
 
