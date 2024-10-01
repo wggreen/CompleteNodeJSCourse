128f6fb31d8eb036787f480fd0cc93971a82984d
William Green <wggreen.nss@gmail.com>
2024-10-01 15:55:09 -0400

    finished chapter 19

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require("yargs")
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 const command = process.argv[2]
 
@@ -20,8 +20,7 @@ yargs.command({
         }
     },
     handler: function (argv){
-        console.log('Title:', argv.title)
-        console.log('Body:', argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
