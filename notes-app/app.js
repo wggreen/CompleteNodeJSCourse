c42e4eb378087ebd561b62372d04d803d3a2a1e8
William Green <wggreen.nss@gmail.com>
2024-10-10 10:52:44 -0400

    finished chapter 22

--- notes-app/app.js
+++ notes-app/app.js
@@ -19,7 +19,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv){
+    handler(argv){
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -34,7 +34,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function(argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -42,7 +42,7 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function() {
+    handler() {
         console.log('Listing out all notes!')
     }
 })
@@ -50,7 +50,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function() {
+    handler() {
         console.log('Reading a note!')
     }
 })
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function() {
+const getNotes = () => {
     return 'Your notes...'
 }
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function(note) {
-        return note.title === title 
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title )
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -24,28 +22,10 @@ const addNote = function(title, body) {
     }
 }
 
-const saveNotes = function(notes) {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
-
-}
-
-const loadNotes = function() {
-    try {
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    } catch (e) {
-        return []
-    }
-}
-
-const removeNote = function(title) {
+const removeNote = (title) => {
     const notes = loadNotes()
 
-    const notesToKeep = notes.filter(function(note) {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title )
 
     if (notes.length > notesToKeep.length)
     {
@@ -56,6 +36,21 @@ const removeNote = function(title) {
     }
 
     saveNotes(notesToKeep)
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
 }
 
 module.exports = {
